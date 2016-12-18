e1845362c25d0bb0beb5cdac048baa7454544a4a
Eryk Sobczak <ersobcz@gmail.com>
2016-12-18 09:44:29 +0100

    Postawienie środowiska.

---
+++ src/PhyloTree.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Blamad on 18.12.2016.
+ */
+
+/**
+ 11. „Kalkulator” drzew filogenetycznych ukorzenionych. Typ A.
+ Drzewa wczytywane są z pliku tekstowego (można użyć jakiegoś gotowego parsera np. formatu NEWICK).
+ Operacje:
+ -  konwersja reprezentacji „rodzina zgodnych klastrów” ⇔ drzewo jako graf z jakąś jego wizualizacją (oraz test poprawności danych tj. „czy podana rodzina była zgodna?”).
+ -  Wyznaczanie odległości topologicznej RF między parą drzew, drzewa konsensusu (o podanym poziomie procentowym) dla zadanego zbioru drzew
+    oraz jej wspólne rozszerzenie (jeśli takie istnieje).
+ - Obcięcie podanego drzewa do drzewa filogenetycznego do zadanego podzbioru liści.
+ */
+
+public class PhyloTree {
+
+    public PhyloTree()
+    {
+
+    }
+
+    public void run()
+    {
+        //No i co to ma robic?
+        //W res znajduja sie przykladowe drzewa w formacie newick.
+    }
+
+    public static void main(String [] args)
+    {
+        System.out.println("Daj Pan 3!");
+        new PhyloTree().run();
+    }
+}
